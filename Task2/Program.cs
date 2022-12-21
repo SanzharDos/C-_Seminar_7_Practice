4d65efcaaa652cd35b3e2c628d7dcb9b4b32396d
Sanzhar <dos.0227@yandex.ru>
2022-12-21 14:35:46 +0600

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-20, 21);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine($"Сгенерирован массив из {array.GetLength(0)} строк и {array.GetLength(1)} столбцов:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("{ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}\t");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+    }
+}
+
+void Find(int[,] array, int num)
+{
+    int check = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+            {
+                check = 1;
+                Console.WriteLine($"Искомое число {num} находится в следующих координатах: строка {i} и столбец {j}");
+            }
+        }
+    }
+    if (check == 0) Console.WriteLine($"Искомое число {num} отсутствует в массиве");
+}
+
+Console.WriteLine("Введите количество строк двумерного массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("Введите искомое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Find(array, num);
+
