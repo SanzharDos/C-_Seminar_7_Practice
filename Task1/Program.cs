868e3be0aed269cd6960fa64c90f388e9be4f4d6
Sanzhar <dos.0227@yandex.ru>
2022-12-21 14:24:23 +0600

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * (10 + 10);
+            array[i, j] = Math.Round(array[i, j], 2);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    Console.WriteLine($"Сгенерирован массив из {array.GetLength(0)} строк и {array.GetLength(1)} столбцов:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("{ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],5}\t");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите количество строк двумерного массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[rows, cols];
+FillArray(array);
+PrintArray(array);
